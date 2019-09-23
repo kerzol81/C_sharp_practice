22836e2944bfe99e3401d134a455338bfaff77d1
Z <kerenyi.nagy.zoltan@gmail.com>
2019-09-23 16:48:06 +0200

    Create used_cars.cs

---
+++ used_cars.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cars_1
+{
+    class Car
+    {
+        string plate;
+        byte state;
+
+        public Car(string plate, byte state)
+        {
+            Plate = plate;
+            State = state;
+        }
+
+        public string Plate
+        {
+            get => plate;
+            set {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    plate = value;
+                }
+                else
+                {
+                    plate = "-------";
+                }
+            }
+        }
+        public byte State
+        {
+            get => state;
+            set {
+                if (0 < value && value < 101)
+                {
+                    state = value;
+                }
+                else
+                {
+                    state = 1;
+                }
+            }
+        }
+
+        public static void PrintWorst(List<Car> cars)
+        {
+            Car worst = cars[0];
+            for (int i = 0; i < cars.Count(); i++)
+            {
+                if (cars[i].state < worst.state)
+                {
+                    worst = cars[i];
+                }
+            }
+            Console.WriteLine($"{worst.plate} is in the worst condition");
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Car> cars = new List<Car>();
+
+            Console.WriteLine("Enter cars data: \t\tpress enter to escape filling");
+            string plate = " ";
+            byte state;
+            do
+            {
+                Console.Write("Plate: ");
+                plate = Console.ReadLine();
+                if (plate != "")
+                {
+                    Console.Write("State: ");
+                    state = Byte.Parse(Console.ReadLine());
+                    Car new_car = new Car(plate, state);
+                    cars.Add(new_car);
+                }
+            } while (plate != "" );
+
+            Car.PrintWorst(cars);
+            Console.ReadLine();
+            
+        }
+    }
+}
