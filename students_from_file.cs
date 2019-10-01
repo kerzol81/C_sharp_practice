5c6dc050552e3d9e416b80a8321afc13fcef22fb
Z <kerenyi.nagy.zoltan@gmail.com>
2019-10-01 14:40:02 +0200

    Create students_from_file.cs

---
+++ students_from_file.cs
@@ -0,0 +1,75 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace names_01
+{
+    class Student
+    {
+        internal string name;
+        internal double average;
+
+        public Student(string name, double average)
+        {
+            this.name = name;
+            this.average = average;
+        }
+
+        public static void ClassAverage(List<Student> students)
+        {
+            double total = 0;
+            foreach (Student s in students)
+            {
+                total += s.average;
+            }
+            Console.WriteLine($"Class average: {total / students.Count}");
+        }
+        
+        public static void Best(List<Student> students)
+        {
+            Student best = students[0];
+            foreach (Student item in students)
+            {
+                if (item.average > best.average)
+                {
+                    best = item;
+                }
+            }
+            Console.WriteLine($"The best student is {best.name}");
+        }
+        internal static void Worst(List<Student> students) 
+        {
+            Student worst = students[0];
+            foreach (Student item in students)
+            {
+                if (item.average < worst.average)
+                {
+                    worst = item;
+                }
+            }
+            Console.WriteLine($"The worst student is {worst.name}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>();
+
+            StreamReader s = new StreamReader("names.txt");
+
+            for (int i = 0; i < File.ReadAllLines("names.txt").Length / 2; i++)
+            {
+                students.Add(new Student(s.ReadLine(), Convert.ToDouble(s.ReadLine())));
+            }
+            
+            Student.ClassAverage(students);
+            Student.Best(students);
+            Student.Worst(students);
+
+            Console.ReadKey();
+        }
+    }
+}
