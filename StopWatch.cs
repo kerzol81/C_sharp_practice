d01fbae5eccd6cbe4235103f3c30d46f039d9fdf
Z <kerenyi.nagy.zoltan@gmail.com>
2019-12-20 22:00:38 +0100

    Create StopWatch.cs

---
+++ StopWatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime start_time;
+        private DateTime stop_time;
+        private bool running;
+
+        public StopWatch()
+        {
+            running = false;
+        }
+        public void Start()
+        {
+            if (running)
+            {
+                throw new InvalidOperationException("instance already started");
+            }
+            else
+            {
+                start_time = DateTime.Now;
+                running = true;
+            }       
+        }
+        public void Stop() {
+            if (!running)
+            {
+                throw new InvalidOperationException("instance is not started");
+            }
+            else
+            {
+                stop_time = DateTime.Now;
+            }
+        }
+        public TimeSpan GetDuration()
+        {
+            this.Stop();
+            return stop_time - start_time;
+        }
+    }
+}
