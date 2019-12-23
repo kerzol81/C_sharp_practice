2fb79ed8f075fc377c58fb552f1281366837e2fa
Z <kerenyi.nagy.zoltan@gmail.com>
2019-12-23 08:23:32 +0100

    Create stackOverflowPost.cs

---
+++ mosh/stackOverflowPost.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace StackOverFlowPost
+{
+    class Post
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Created { get; private set; }
+        private int _vote;
+        public int Vote
+        {
+            get { return _vote; }
+            set {
+                
+                if (value > 0)
+                {
+                    _vote = value;
+                }
+                else
+                {
+                    _vote = 0;
+                }
+            }
+
+        }
+
+        public Post(string title, string description, DateTime created)
+        {
+            Title = title;
+            Description = description;
+            Created = created;
+            Vote = 0;
+        }
+
+        public void UpVote()
+        {
+            Vote++;
+        }
+
+        public void DownVote()
+        {
+            Vote--;
+        }
+        public void DisplayCurrentVote() {
+            Console.WriteLine($"Current vote: {Vote}");
+        }
+    }
+}
