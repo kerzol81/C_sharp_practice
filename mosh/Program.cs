eea601644ce98ac629b346ab26b08e091a899506
Z <kerenyi.nagy.zoltan@gmail.com>
2019-12-24 15:42:27 +0100

    Add files via upload

---
+++ mosh/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StopWatch.Start();
+            Thread.Sleep(100);
+            StopWatch.LapStop();
+            Thread.Sleep(200);
+            StopWatch.LapStop();
+
+            StopWatch.Stop();
+            StopWatch.DisplayAll();
+
+            Console.ReadLine();
+        }
+    }
+}
