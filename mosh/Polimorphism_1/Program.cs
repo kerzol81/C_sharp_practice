919ca4f3a9a35730cbdc8cbde9053f9963414964
Z <kerenyi.nagy.zoltan@gmail.com>
2020-01-01 18:00:18 +0100

    Update Program.cs

--- mosh/Polimorphism_1/Program.cs
+++ mosh/Polimorphism_1/Program.cs
@@ -1,9 +1,23 @@
+using System;
+
 namespace Polimorphism_Excercise
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var s = new SQLConnection("sql_test", new TimeSpan(1, 1, 1));
+
+            s.Open();
+            s.Close();
+
+            var d = new OracleConnection("oracle_test", new TimeSpan(1, 2, 1));
+
+            d.Open();
+            d.Close();       
+
+            Console.ReadKey();
+            
         }
     }
 }
