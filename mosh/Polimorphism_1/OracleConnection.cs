6a1ca214b0956cef6085e1fdd62e5f875251a7d6
Z <kerenyi.nagy.zoltan@gmail.com>
2020-01-01 18:04:36 +0100

    Add files via upload

---
+++ mosh/Polimorphism_1/OracleConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Polimorphism_Excercise
+{
+    internal class OracleConnection : DbConnection
+    {
+        string name = "oracle connection";
+        public OracleConnection(string connectionString, TimeSpan timeOut)
+            :base(connectionString, timeOut)
+        {
+
+        }
+        public override void Open()
+        {
+            Console.WriteLine($"{name} opening");
+        }
+        public override void Close()
+        {
+            Console.WriteLine($"{name} closing");
+        }
+    }
+}
