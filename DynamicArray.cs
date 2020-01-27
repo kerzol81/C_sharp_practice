f237b322ed291aa403f3a04309fe25e126848ac2
Z <kerenyi.nagy.zoltan@gmail.com>
2020-01-27 08:21:03 +0100

    Update DynamicArray.cs

--- DynamicArray.cs
+++ DynamicArray.cs
@@ -1,157 +1,51 @@
 using System;
 
-namespace Arrays
+namespace DynamicArrayImplementation
 {
     class Array
     {
-        int[] items;
-        int counter;
+        private int[] _items;
+        private int _counter = 0;       // will be incremented when adding an item
 
         public Array(int lenght)
         {
-            items = new int[lenght];
-        }
-        internal void Print()
-        {
-            for (int i = 0; i < counter; i++)
-            {
-                Console.WriteLine(items[i]);
-            }
+            _items = new int[lenght];
         }
 
-        internal void Append(int item)
+        public void RemoveAt(int index)
         {
-            //resize if full
-            if (items.Length == counter)
+            if (index <= _counter && index >= 0)
             {
-                int[] temp = new int[counter * 2];
-                for (int i = 0; i < counter; i++)
+                for (int i = index; i < _counter; i++)
                 {
-                    temp[i] = items[i];
+                    _items[i] = _items[i + 1];
                 }
-                items = temp;
+                _counter--;
             }
-
-            // add to the end, and increment
-            items[counter] = item;
-            counter++;
-
+            else
+                throw new ArgumentOutOfRangeException("No such index");
         }
 
-        internal void Remove(int index)
+        public void Print()
         {
-            // validate index
-            if (index < 0 || index >= counter)
+            for (int i = 0; i < _counter; i++)
             {
-                throw new ArgumentOutOfRangeException("no such index");
+                Console.WriteLine(_items[i]);
             }
-            /*  
-                10, 20, 30, 40, 50   if index is 1, then
-                10, X, 30, 40, 50    count from index until the end
-                10, 30, 40, 50       cut off the end
-             */
-            for (int i = index; i < counter; i++)
-            {
-                items[i] = items[i + 1];
-            }
-            counter--;
-
-
         }
 
-        internal void Insert(int index, int item)
+        public void Append(int item)
         {
-            //validate
-            if (index < 0 || index > counter)
+            if (_counter == _items.Length)
             {
-                throw new ArgumentOutOfRangeException("no such index");
-            }
-            
-            int[] temp = new int[counter + 1];
-            for (int i = 0; i < index; i++)
-            {
-                temp[i] = items[i];                
-            }
-            
-            temp[index] = item;
-            
-            for (int i = index; i < counter; i++)
-            {
-                temp[i + 1] = items[i];
-            }
-            counter++;
-            items = temp;
-        }
+                int[] temp = new int[_counter * 2];
 
-        internal int IndexOf(int item)
-        {
-            for (int i = 0; i < counter; i++)
-            {
-                if (items[i] == item)
-                {
-                    return i;
-                }
-            }
-            // if it wasnt found
-            return -1;
-        }
+                for (int i = 0; i < _counter; i++)
+                    temp[i] = _items[i];
 
-        internal int MaxItem()
-        {
-            // Runtime complexity id O(n)
-            int max = items[0];
-            for (int i = 1; i < counter; i++)
-            {
-                if (items[i] > max)
-                {
-                    max = items[i];
-                }
+                _items = temp;
             }
-            return max;
-
-        }
-        internal int MinItem()
-        {
-            int min = items[0];
-            for (int i = 1; i < counter; i++)
-            {
-                if (items[i] < min)
-                {
-                    min = items[i];
-                }
-            }
-            return min;
-        }
-        internal void Intersect(Array other)
-        {
-            // Runtime complexity: O(n^2)
-            Array commons = new Array(counter);
-            for (int i = 0; i < counter; i++)
-            {
-                for (int j = 0; j < other.counter; j++)
-                {
-                    if (items[i] == other.items[j])
-                    {
-                        commons.Append(items[i]);
-                    }
-                }
-            }
-            for (int i = 0; i < commons.counter; i++)
-            {
-                Console.WriteLine(commons.items[i]);
-            }
-        }
-        internal void Reverse()
-        {
-            // Runtime complexity : O(n)
-            int[] temp = new int[counter];
-
-            for (int i = counter - 1, j = 0; i >= 0; i--, j++)
-            {
-                temp[j] = items[i];
-
-            }
-            items = temp;
+            _items[_counter++] = item;
         }
     }
 }
